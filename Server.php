3ae27540016c16dfdd3367b79043f631926f9669
d_ito <ito@gizumo-inc.jp>
2021-07-27 10:58:18 +0900

    add: ratchet使用のWebSocketサーバスクリプト

---
+++ Server.php
@@ -0,0 +1,19 @@
+<?php
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+require_once './vendor/autoload.php';
+require_once './Chat.php';
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8333
+);
+
+$server->run();
